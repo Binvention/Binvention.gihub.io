6a83f987aad08512dfd31c4e673c30cdceef2096
Binvention <131bairdbk+work@gmail.com>
2019-04-11 03:32:51 -0600

    debugging

--- index.js
+++ index.js
@@ -1,100 +1,45 @@
-define(["exports", "react", "./projects/clock.jsx"], function (exports, _react, _clock) {
-   "use strict";
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-   Object.defineProperty(exports, "__esModule", {
-      value: true
-   });
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-   var _react2 = _interopRequireDefault(_react);
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-   var _clock2 = _interopRequireDefault(_clock);
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-   function _interopRequireDefault(obj) {
-      return obj && obj.__esModule ? obj : {
-         default: obj
-      };
+import React from "react";
+import Clock from "./projects/clock.jsx";
+
+var App = function (_React$Component) {
+   _inherits(App, _React$Component);
+
+   function App(props) {
+      _classCallCheck(this, App);
+
+      return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
    }
 
-   function _classCallCheck(instance, Constructor) {
-      if (!(instance instanceof Constructor)) {
-         throw new TypeError("Cannot call a class as a function");
-      }
-   }
-
-   var _createClass = function () {
-      function defineProperties(target, props) {
-         for (var i = 0; i < props.length; i++) {
-            var descriptor = props[i];
-            descriptor.enumerable = descriptor.enumerable || false;
-            descriptor.configurable = true;
-            if ("value" in descriptor) descriptor.writable = true;
-            Object.defineProperty(target, descriptor.key, descriptor);
-         }
-      }
-
-      return function (Constructor, protoProps, staticProps) {
-         if (protoProps) defineProperties(Constructor.prototype, protoProps);
-         if (staticProps) defineProperties(Constructor, staticProps);
-         return Constructor;
-      };
-   }();
-
-   function _possibleConstructorReturn(self, call) {
-      if (!self) {
-         throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
-      }
-
-      return call && (typeof call === "object" || typeof call === "function") ? call : self;
-   }
-
-   function _inherits(subClass, superClass) {
-      if (typeof superClass !== "function" && superClass !== null) {
-         throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
-      }
-
-      subClass.prototype = Object.create(superClass && superClass.prototype, {
-         constructor: {
-            value: subClass,
-            enumerable: false,
-            writable: true,
-            configurable: true
-         }
-      });
-      if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
-   }
-
-   var App = function (_React$Component) {
-      _inherits(App, _React$Component);
-
-      function App(props) {
-         _classCallCheck(this, App);
-
-         return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
-      }
-
-      _createClass(App, [{
-         key: "save",
-         value: function save(image) {}
-      }, {
-         key: "render",
-         value: function render() {
-            return _react2.default.createElement(
+   _createClass(App, [{
+      key: "save",
+      value: function save(image) {}
+   }, {
+      key: "render",
+      value: function render() {
+         return React.createElement(
+            "div",
+            null,
+            React.createElement("div", { id: "menu" }),
+            React.createElement(
                "div",
-               null,
-               _react2.default.createElement("div", { id: "menu" }),
-               _react2.default.createElement(
-                  "div",
-                  { style: { display: "none" } },
-                  _react2.default.createElement(_clock2.default, { radius: 50 })
-               )
-            );
-         }
-      }]);
+               { style: { display: "none" } },
+               React.createElement(Clock, { radius: 50 })
+            )
+         );
+      }
+   }]);
 
-      return App;
-   }(_react2.default.Component);
+   return App;
+}(React.Component);
 
-   exports.default = App;
+export default App;
 
-   ReactDOM.render(App, document.getElementById("root"));
-});
+ReactDOM.render(App, document.getElementById("root"));
--- projects/projects.js
+++ projects/projects.js
@@ -1,5 +1,1 @@
-define([], function () {
-  "use strict";
-
-  console.log("hello world");
-});
+console.log("hello world");
